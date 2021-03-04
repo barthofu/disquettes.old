e2837514974dbdfdad6550479ceacc03dab3cc2e
LazareC <lazarela17@gmail.com>
2021-03-04 21:44:53 +0100

    work on admin dashboard

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,14 +1,25 @@
 const express = require('express')
-
-
 let router = express.Router();
+
+const pages = ['index', 'list', 'submit', '/', 'overview'];
 
 router
     .get('/', (req, res) => {
-
-        res.render("admin/layout", { page: "index", title: "Disquette Admin", admin: false })
+        console.log(req.baseUrl)
+        res.redirect(req.baseUrl +
+            "/overview")
     })
-    .get('/', (req, res) => getDisquette(req, res))
-    .post('/', (req, res) => getDisquette(req, res))
+    .get(['/:page'], (req, res) => sender(req, res, req.params.page))
+
+.post((req, res) => getDisquette(req, res));
+
+function sender(req, res, page) {
+    if (pages.includes(page)) {
+        return res.render("admin/layout", { page: page, title: "Disquette Admin", admin: false, user: { unsername: "LeZ", profil_url: "https://via.placeholder.com/150" } })
+    } else {
+
+        res.status(404).send('Not Found!');
+    }
+}
 
 module.exports = router;
