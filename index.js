54e5f4faf05717c8b13843136536333933bdaca2
LazareC <lazarela17@gmail.com>
2021-05-04 13:08:14 +0200

    .credentials.json to .env

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const app = require('./src/app');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const PORT = process.env.PORT || 4000;
+const MONGOOSE_URL = process.env.MONGOOSE_URL;
+const MONGOOSE_PORT = process.env.MONGOOSE_PORT;
+const MONGOOSE_USERNAME = process.env.MONGOOSE_USERNAME;
+const MONGOOSE_PASSWORD = process.env.MONGOOSE_PASSWORD;
+const MONGOOSE_DATABASE = process.env.MONGOOSE_DATABASE;
 
-
-const config = require('./.credentials.json');
-
+const config = {
+	username: MONGOOSE_USERNAME,
+	password: MONGOOSE_PASSWORD,
+	hosts: [
+		{
+			host: MONGOOSE_URL,
+			port: MONGOOSE_PORT,
+		},
+	],
+	database: MONGOOSE_DATABASE,
+	options: {
+		authSource: 'admin',
+	},
+};
 mongoose
-	.connect(require('mongodb-uri').format(config.db), {
+	.connect(require('mongodb-uri').format(config), {
 		keepAlive: 1,
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
